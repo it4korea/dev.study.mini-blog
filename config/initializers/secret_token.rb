7ece8095a9bc2e93ba36f665fbb950d3a7db630a
Kim Daekwon <propellerheaven@gmail.com>
2013-08-06 18:13:59 +0900

    rails 4.0.0 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stub::Application.config.secret_key_base = 'ad27019a233e057ffd9250b8b5d28f3b1ac5215d489274e7aba0aa86bad4a49e56586558c59329c1a2e3d87d59cc7d1bee4960603dc84828eac03b0bf9ea7ed6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stub::Application.config.session_store :cookie_store, key: '_stub_session'
